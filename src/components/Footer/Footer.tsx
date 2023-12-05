d9f6f94a6a6099446ba129565e4cb5d9b5ea4308
Binaya Bajracharya <binaya.bajrashakya@gmail.com>
2023-12-05 13:00:31 +0545

    Fix opacity issue in hero map and remove footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,18 +13,6 @@ const Footer = () => {
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((columns, i) => (
-              <FooterColumn title={columns.title} key={i}>
-                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
-                      {link}
-                    </Link>
-                  ))}
-                </ul>
-              </FooterColumn>
-            ))}
-
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
                 {FOOTER_CONTACT_INFO.links.map((link) => (
